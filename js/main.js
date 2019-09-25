c3f2a4a1776f80950a4a3f4fa880b5389c0b6879
crappydeveloper <artemka666@mail.ru>
2019-09-25 15:19:21 +0300

    changed calculation principle of photos property

--- js/main.js
+++ js/main.js
@@ -1,29 +1,3 @@
-/*
-{
-  "author": {
-    "avatar": строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
-  },
-
-  "offer": {
-    "title": строка, заголовок предложения
-    "address": строка, адрес предложения. Для простоты пусть пока представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
-    "price": число, стоимость
-    "type": строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
-    "rooms": число, количество комнат
-    "guests": число, количество гостей, которое можно разместить
-    "checkin": строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
-    "checkout": строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
-    "features": массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
-    "description": строка с описанием,
-    "photos": массив строк случайной длины, содержащий адреса фотографий "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-  },
-
-  "location": {
-    "x": случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-    "y": случайное число, координата y метки на карте от 130 до 630.
-  }
-}
-*/
 var map = document.querySelector(".map");
 map.classList.remove("map--faded");
 
@@ -33,14 +7,14 @@ function createArray() {
   var types = ["palace", "flat", "house", "bungalo"];
 
   function getRandomItems(arr) {
-    return Math.floor(Math.random() * arr.length);
+    return Math.floor(Math.random() * (arr.length + 1)) - 1;
   }
 
   function getFeatures() {
     var chosenFeatures = [];
     var features = ["wifi", "dishwasher", "parking", "washer", "elevator", "conditioner"];
 
-    for (var i = 0; i < getRandomItems(features); i++) {
+    for (var i = 0; i <= getRandomItems(features); i++) {
       chosenFeatures.push(features[i]);
     }
 
@@ -48,14 +22,13 @@ function createArray() {
   }
 
   function getPhotos() {
-    var sourcePhotosArr = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
     var photosArr = [];
+    var sourcePhotosArr = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
 
-    photosArr.length = Math.floor(Math.random() * 10);
-
-    for (var i = 0; i < sourcePhotosArr.length - 1; i++) {
-      photosArr.push(sourcePhotosArr[i]);
+    for (var i = 0; i <= getRandomItems(sourcePhotosArr); i++) {
+      photosArr[i] = sourcePhotosArr[i];
     }
+
     return photosArr;
   }
 
@@ -75,7 +48,7 @@ function createArray() {
         checkout: "12:00",
         features: getFeatures(),
         description: "Строка с описанием",
-        "photos": getPhotos(); 
+        "photos": getPhotos()
       },
       location: {
         x: Math.floor(Math.random() * mapWidth),
