cc3f5adeee5ec13d8e393ff8b84286a220b686fd
Artem Lapkin <artemka666@mail.ru>
2019-11-10 16:42:50 +0300

    WIP on the task

--- js/xhr.js
+++ js/xhr.js
@@ -29,7 +29,36 @@
     xhr.send();
   }
 
+  function upload(data, successHandler, errorHandler) {
+    var URL = 'https://js.dump.academy/keksobooking';
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        successHandler(xhr.response);
+      } else {
+        errorHandler('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      errorHandler();
+    });
+
+    xhr.addEventListener('timeout', function () {
+      errorHandler();
+    });
+
+    xhr.timeout = 4000;
+
+    xhr.open('POST', URL);
+    xhr.send(data);
+  }
+
   window.xhr = {
     load: load,
+    upload: upload,
   };
 })();
